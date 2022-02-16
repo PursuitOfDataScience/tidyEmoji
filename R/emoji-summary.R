97225052875d8563ccd3388c0375008c3705823c
Youzhi Yu <yuy@bgsu.edu>
2022-02-15 19:00:41 -0500

    added data and function

--- R/emoji-summary.R
+++ R/emoji-summary.R
@@ -1,16 +1,22 @@
-#' Emoji Summary Tibble
+#' Emoji summary tibble
 #'
-#' @param tweet_tbl A dataframe/tibble containing tweets.
-#' @param tweet_text The column that is the tweet column.
+#' When having a Twitter dataframe/tibble at hand, it should be nice to know
+#' how many Tweets contain Emojis. This is the right time to use this function.
+#' What is worth noting is that it does not matter whether a Tweet has one Emoji
+#' or ten Emojis, the function only counts it once and returns a tibble that
+#' summarizes the number of Tweets containing at least one Emoji and the total
+#' number of Tweets presented in the dataframe/tibble.
 #'
-#' @return A summary tibble including # of tweets in total and # of tweets that
+#' @param tweet_tbl A dataframe/tibble containing tweets/text.
+#' @param tweet_text The tweet/text column.
+#'
+#' @return A summary tibble including # of Tweets in total and # of Tweets that
 #' have at least one Emoji.
 #'
 #' @import dplyr
 #' @import emoji
 #' @import stringr
 #' @import tibble
-#' @import rlang
 #' @export
 #'
 
@@ -21,14 +27,40 @@ emoji_summary <- function(tweet_tbl, tweet_text){
 
   num_emoji_tweets <- tweet_tbl %>%
     dplyr::filter(stringr::str_detect({{ tweet_text }},
-                      emoji::emojis %>%
-                        dplyr::filter(!stringr::str_detect(name, "keycap: \\*")) %>%
-                        dplyr::pull(emoji) %>%
-                        paste(., collapse = "|"))) %>%
+                                      emoji::emojis %>%
+                                        dplyr::filter(!stringr::str_detect(name, "keycap: \\*")) %>%
+                                        dplyr::pull(emoji) %>%
+                                        paste(., collapse = "|"))) %>%
     dim() %>%
     .[1]
 
   return(tibble::tibble(emoji_tweets = num_emoji_tweets,
                         total_tweets = num_tweets))
+
+}
+
+
+
+
+
+#' Emoji Text/Tweets Output
+#'
+#' When users just want to focus on Tweets containing Emoji(s),
+#' \code{emoji_tweets} filters out non-Emoji rows and only returns rows that
+#' have at least one Emoji.
+#'
+#' @inheritParams emoji_summary
+#'
+#' @return A dataframe/tibble containing only text with at least one Emoji
+#' @export
+
+emoji_tweets <- function(tweet_tbl, tweet_text){
+
+  tweet_tbl %>%
+    dplyr::filter(stringr::str_detect({{ tweet_text }},
+                                      emoji::emojis %>%
+                                        dplyr::filter(!stringr::str_detect(name, "keycap: \\*")) %>%
+                                        dplyr::pull(emoji) %>%
+                                        paste(., collapse = "|")))
 
 }
--- R/tidyEmoji.R
+++ R/tidyEmoji.R
@@ -4,9 +4,13 @@
 #'
 #' @docType package
 #' @name tidyEmoji
-#' @importFrom dplyr %>%
-#' @importFrom purrr %||%
+#' @import utils
 NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "name"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c(".",
+                                                        "name",
+                                                        "emoji_name",
+                                                        "unicode",
+                                                        "emoji_category",
+                                                        "emoji_unicode_crosswalk"))
